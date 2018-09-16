f7f63eaf810db4a5324980cd57ad50084989e512
Diogo Henrique <diogo.henrique.df@gmail.com>
2018-09-16 20:23:23 -0300

    Iniciando repositorio da video aula(Parte 1 até a Parte 5)

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,12 @@
+'use strict'
+
+class UserController {
+
+  async login ({ request, auth}){
+    const {email, password} = request.all();
+    const {token} = await auth.attempt(email, password);
+    return token;
+  }
+}
+
+module.exports = UserController;
---
+++ database/migrations/1537050662290_users_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class UsersSchema extends Schema {
+  up () {
+    this.create('users', (table) => {
+      table.increments();
+      table.string('username',80).unique();
+      table.string('email').unique();
+      table.string('password',80).notNullable();
+      table.integer('age');
+      table.timestamps();
+    })
+  }
+
+  down () {
+    this.drop('users')
+  }
+}
+
+module.exports = UsersSchema
